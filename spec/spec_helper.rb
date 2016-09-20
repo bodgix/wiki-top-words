6d85f9eb3472787cf7d43609fa94ac822010cb12
Bogdan Katyński <bogdan.katynski@workday.com>
2016-09-20 03:36:37 +0100

    Add part of the tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+# encoding: UTF-8
+
+require 'rspec'
+require 'simplecov'
+SimpleCov.start
---
+++ spec/unit/wiki_page_spec.rb
@@ -0,0 +1,41 @@
+# encoding: UTF-8
+
+require_relative '../spec_helper'
+require_relative '../../lib/wiki_page'
+require 'pry-byebug'
+
+describe WikiTopWords::WikiPage do
+  let(:page_id) { 42 }
+  let(:subject) { described_class.new(page_id) }
+  let(:content) do
+    File.read("spec/data/wikipedia_#{page_id}.json")
+  end
+  describe '#new' do
+    it 'initializes an object and returns it' do
+      expect(subject).to be_a(described_class)
+      expect(subject.page_id).to eql(page_id)
+    end
+  end
+
+  describe 'content' do
+    let(:page_content) { '{"test": 42}' }
+    it 'gets the page and parse JSON' do
+      expect(subject).to receive(:get_page) { page_content }
+      expect(subject.content).to eql(JSON.parse(page_content))
+    end
+  end
+
+  describe 'title' do
+    it 'returns the title key from the content json' do
+      expect(subject).to receive(:get_page) { content }
+      expect(subject.title).to eql('Test')
+    end
+  end
+
+  describe 'extract' do
+    it 'returns the extract key from the content json' do
+      expect(subject).to receive(:get_page) { content }
+      expect(subject.extract).to eql('word1 and word2 word2 word1 word3')
+    end
+  end
+end
