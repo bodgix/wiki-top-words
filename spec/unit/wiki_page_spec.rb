c573bf3235ba11d568686b2cbcb92dc9e422ac7d
Bogdan Katyński <bogdan.katynski@workday.com>
2016-09-21 00:33:39 +0100

    Add a unit test for get_page

--- spec/unit/wiki_page_spec.rb
+++ spec/unit/wiki_page_spec.rb
@@ -3,6 +3,8 @@
 require_relative '../spec_helper'
 require_relative '../../lib/wiki_page'
 
+require 'pry-byebug'
+
 describe WikiTopWords::WikiPage do
   let(:page_id) { 42 }
   let(:subject) { described_class.new(page_id) }
@@ -53,5 +55,46 @@ describe WikiTopWords::WikiPage do
   describe 'to_a' do
     let(:method) { subject.to_a }
     it_behaves_like 'article to words'
+  end
+
+  describe 'get_page' do
+    let(:code) { 200 }
+    let(:response) do
+      r = double
+      expect(r).to receive(:code).at_least(:once) { code }
+      r
+    end
+    before do
+      default_query = {
+        action:       'query',
+        prop:         'extracts',
+        explaintext:  true,
+        format:       'json'
+      }
+      expect(described_class).to receive(:get).\
+        with('/w/api.php', query: default_query.merge(pageids: page_id)) { response }
+
+      # get_page is a private method. we need a wrapper public method
+      subject.instance_eval do
+        def call_get_page
+          get_page
+        end
+      end
+    end
+
+    context 'when wikipedia returns 200 OK' do
+      it 'does a HTTP GET and returns the page content' do
+        expect(response).to receive(:body) { 'test' }
+
+        expect(subject.call_get_page).to eql('test')
+      end
+    end
+
+    context 'when wikipedia returns a non-200 code' do
+      let(:code) { 500 }
+      it 'does a HTTP get and raises an error' do
+        expect { subject.call_get_page }.to raise_error(RuntimeError)
+      end
+    end
   end
 end
